e867ab4bb2727beb9fa45f129735e23cc47b7c4f
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2019-04-16 01:37:14 +0200

    Add Grid class
    Added Grid class with basic constructors and methods for creating and displaying the grid in the console.

--- src/Grid.java
+++ src/Grid.java
@@ -1,2 +1,56 @@
 public class Grid {
+    // ATTRIBUTES
+    private int height = 10;        // number of rows of the grid
+    private int width  = 14;        // number of the columns of the grid
+    private Tile[][] tile;          // tiles on this grid
+
+    // CONSTRUCTORS
+
+    /**
+     * Default Grid with default size (10x14) and data
+     */
+    public Grid() {
+        instantiate();
+    }
+
+    /**
+     * Default Grid with set size
+     */
+    public Grid(int height, int width) {
+        this.height = height;
+        this.width = width;
+        instantiate();
+    }
+
+    // METHODS
+    /**
+     * Fills in the Grid with tiles to defaults and assigns numbers
+     */
+    private void instantiate() {
+        // set the size of the grid
+        this.tile = new Tile[height][width];
+
+        int num = 0;
+        for(int i = 0; i < tile.length; i++) {
+            for(int j = 0; j < tile[i].length; j++) {
+                // create the grid, assign number and owning grid
+                tile[i][j] = new Tile(num++, this);
+            }
+        }
+    }
+
+    /**
+     * Prints the Grid to the console
+     * FORMAT: height*width grid, displaying tile numbers
+     *         NOTE: every number starts with a tab
+     */
+    public void printGridNums() {
+        for(int i = 0; i < tile.length; i++) {
+            for(int j = 0; j < tile[i].length; j++) {
+                System.out.print("\t" + tile[i][j].tileNum);
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/MapMaker.java
+++ src/MapMaker.java
@@ -2,6 +2,9 @@ import javax.swing.JFrame;
 
 public class MapMaker {
     public static void main(String[] args) {
+        Grid grid = new Grid();
+        grid.printGridNums();
+
         GUI gui = new GUI();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gui.setSize(720, 600);
