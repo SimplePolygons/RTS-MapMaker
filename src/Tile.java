0b754ec6d49c0f620dbd87fc14c7dde4a7c9d539
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2019-04-16 01:10:26 +0200

    Basic Tile constructor
    Tile class now has the basic constructor with most necesarry attributes, such as the orientation  and states of the road.
    
    TODO: barricade and material implementations.

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,42 @@
 public class Tile {
+    // CONSTANTS
+    // orientation
+    private int NORTH = 0;          // default
+    private int EAST  = 1;
+    private int SOUTH = 2;
+    private int WEST  = 3;
+
+    // owning player
+    private int NONE       = 0;     // default
+    private int PLAYER_ONE = 1;
+    private int PLAYER_TWO = 2;
+    private int SPECIAL    = 3;
+
+    // ATTRIBUTES
+    boolean isRoad;         // whether the Tile is a road tile or building tile
+    int orientation;        // orientation of the tile (based on constants)
+    int tileNum;            // number assigned to the tile
+    int owner;              // owning player (or notice of a special tile)
+    Grid owningGrid;        // Grid to which the Tile belongs to
+    // TODO: barricades
+    // TODO: material
+
+    // CONSTRUCTORS
+
+    /**
+     * Basic Tile information, set to defaults
+     * @param tileNum       number assigned to the tile
+     * @param owningGrid    Grid to which the Tile belongs to
+     */
+    public Tile(int tileNum, Grid owningGrid) {
+        this.isRoad = true;
+        this.orientation = NORTH;
+        this.tileNum = tileNum;
+        this.owner = NONE;
+        this.owningGrid = owningGrid;
+        // TODO: barricades
+        // TODO: material
+    }
+
+    // METHODS
 }
