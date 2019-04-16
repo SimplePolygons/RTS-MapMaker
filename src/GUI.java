d4cb2302ff1703baed98aaed6a4a4488476eff04
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2019-04-16 13:02:18 +0200

    GUI changes (heigth&width TF and Grid Resize Button)
    Added TextFields for manual setting for Grid size and a Button to create the grid.
    Currently, the TextFields are just there visually.
    The Button spawns new Grid Tiles, but does not remove the old ones. This will be implemented next.
    Changing the Grid size will remove all previous changes to the map, so be careful.

--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,12 @@
 import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.TextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class GUI extends JFrame {
     private static Grid grid;
@@ -14,49 +15,64 @@ public class GUI extends JFrame {
     public GUI() {
         super("Map Maker");
         setLayout(new FlowLayout());
-        /*
-        gumb = new JButton("NEW GAME");
-        gumb.setBackground(Color.WHITE);    //to je za barvo gumba, preden je kliknen
-        gumb.setForeground(Color.BLUE); // to je za besedilo
-        add(gumb);
-        */
 
-        // create grid
-        grid = new Grid();
-        int h = grid.h();
-        int w = grid.w();
+        //
+        TextField tf_height = new TextField();
+        TextField tf_width  = new TextField();
+        add(tf_height);
+        add(tf_width);
 
-        tiles = new JButton[h][w];          // set up the corresponding array of Buttons
-
-        // add action listener
-        // TODO: improve
-        ActionListener listener = new ActionListener(){
+        JButton gridResize = new JButton("Resize grid");
+        gridResize.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent event){
-                // JOptionPane.showMessageDialog(null, String.format("%s", event.getActionCommand()));
-                // System.out.println(event.getActionCommand());
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("pressed");
 
-                // get the location of the pressed button
-                int x = 0;
-                int y = 0;
+                // create grid
+                grid = new Grid();
+                int h = grid.h();
+                int w = grid.w();
 
-                outerloop:
-                for(int i = 0; i < h; i++){
-                    for(int j = 0; j < w; j++){
-                        if(event.getSource() == tiles[i][j]){
-                            x = i;
-                            y = j;
-                            break outerloop;
+                tiles = new JButton[h][w];          // set up the corresponding array of Buttons
+
+                // add action listener
+                // TODO: improve
+                ActionListener listener = new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent event){
+                        // JOptionPane.showMessageDialog(null, String.format("%s", event.getActionCommand()));
+                        // System.out.println(event.getActionCommand());
+
+                        // get the location of the pressed button
+                        int x = 0;
+                        int y = 0;
+
+                        outerloop:
+                        for(int i = 0; i < h; i++){
+                            for(int j = 0; j < w; j++){
+                                if(event.getSource() == tiles[i][j]){
+                                    x = i;
+                                    y = j;
+                                    break outerloop;
+                                }
+                            }
                         }
+                        System.out.println("Tile pressed at: (" + x + ", " + y + ")");
+
+                        // TODO: actions
                     }
-                }
-                System.out.println("Tile pressed at: (" + x + ", " + y + ")");
+                };
 
-                // TODO: actions
+                displayGrid(h, w, listener);
+                System.out.println("Grid displayed");
+
+                // refresh GUI
+                repaint();
+                revalidate();
             }
-        };
+        });
+        add(gridResize);
 
-        displayGrid(h, w, listener);
     }
 
     /**
