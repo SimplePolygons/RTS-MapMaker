1dcdaf5f52d8ebec5fd827cc459600304ea5d550
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2019-05-24 11:00:08 +0200

    Add Org.json library
    This library will be used instead of javax.json, due to the fact that I could not implement it and the new library is sure to work.

--- src/JsonMaker.java
+++ src/JsonMaker.java
@@ -1,18 +1,28 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import libs.java.javax.json.*;
+import org.json.*;
 
-public class JsonMaker implements ActionListener {
-    public JsonMaker(Grid g){}
+public class JsonMaker implements ActionListener  {
+    // ATTRIBUTES
+    final String DEFAULT_NAME = "Map";
 
+    String name;
+    Grid grid;
+
+    // CONSTRUCTORS
+    public JsonMaker(String name, Grid grid) {
+        this.name = name;
+        this.grid = grid;
+    }
+
+    // METHODS
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO: try if the current file name is good
         // set to default if not
 
         // prepare JSON
-        JsonObject json = Json.createObjectBuilder()
-                .add("heigth", )
+        JSONObject jsonObj = new JSONObject();
 
         // TODO: save grid to .json
     }
