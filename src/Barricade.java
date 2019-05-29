42612bc23189d98b3c9976b630254754b3fde472
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2019-05-29 09:29:29 +0200

    Add Barricade class with basic constructor

---
+++ src/Barricade.java
@@ -0,0 +1,19 @@
+public class Barricade {
+    // ATTRIBUTES
+    int type;
+    int hp;
+    int mesh_id;
+
+    // CONSTRUCTORS
+    /**
+     * Basic Barricade information, set to defaults
+     * @param type      Type of the Barricade (with constants)
+     * @param hp        Amount of health points this barricade still hsa
+     * @param mesh_id
+     */
+    public Barricade(int type, int hp, int mesh_id) {
+        this.type = type;
+        this.hp = hp;
+        this.mesh_id = mesh_id;
+    }
+}
