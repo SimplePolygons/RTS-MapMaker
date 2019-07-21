8d1481fdf45da667a9e5fa408738132b328608c2
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2019-07-21 16:26:41 +0200

    FRONTEND: Changed GUI window size

--- src/MapMaker.java
+++ src/MapMaker.java
@@ -4,7 +4,7 @@ public class MapMaker {
     public static void main(String[] args) {
         GUI gui = new GUI();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.setSize(1000, 600);
+        gui.setSize(1000, 700);
         gui.setVisible(true);
     }
 }
